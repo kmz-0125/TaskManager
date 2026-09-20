1af7539a835aca012cfad36683e5a19f85c855cc
km <km_1106@outlook.jp>
2026-09-20 17:54:02 +0900

    日時表示をJSTに統一、タスク編集のキャンセル遷移先を詳細画面に変更

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManager.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        // 2つ目の引数:書式を指定する文字列
+        public static string ToJstString(this DateTime dateTime, string format)
+        {
+            // 東京標準時刻を取得
+            var jstZone = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+            // 引数で受け取ったdateTimeを東京標準時刻のタイムゾーンに変換
+            var jstDateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime, jstZone);
+
+            return jstDateTime.ToString(format);
+        }
+    }
+}
